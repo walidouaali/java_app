e5321404bd18a94dacf6b7521649658388768805
ouaali <walid.ouaali@imt-atlantique.fr>
2025-12-14 18:31:13 +0100

    fixing project

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM openjdk:8-jre-alpine
+FROM eclipse-temurin:11-jre
 
 EXPOSE 8080
 
-COPY ./build/libs/my-app-1.0-SNAPSHOT.jar /usr/app/
+# Copier le jar
+COPY ./build/libs/*.jar /usr/app/
+
 WORKDIR /usr/app
 
-ENTRYPOINT ["java", "-jar", "my-app-1.0-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-jar", "java-app-1.0-SNAPSHOT.jar"]
---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Le projet est fonctionnel ! Bienvenue sur l'endpoint de test.";
+    }
+}
